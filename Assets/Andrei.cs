89dfa2d8c60fc515781d06f78d7de2c2b0328bd6
Raezla <affaokami@hotmail.com>
2016-09-27 13:58:51 +0300

    Andrein tarina
    Andrein tarina upittu

--- Assets/Andrei.cs
+++ Assets/Andrei.cs
@@ -16,23 +16,30 @@ public class Andrei : NPC
         page3 = new DialogPage();
         page4 = new DialogPage();
         page5 = new DialogPage();
+        page5 = new DialogPage();
 
-        page1.SetText("TÖTTÖRÖÖ");
-        page1.SetReply(0, "REPLY1", page2);
-        page1.SetReply(1, "REPLY2", page3);
+        page1.SetText("Jahashh... Mikässhh miässh she shinä olethh?");
+        page1.SetReply(0, "Pate vaan, tarvin rahaa", page2);
+        page1.SetReply(1, "Olen Pate, entäs sinä?", page3);
 
-        page2.SetText("REPLY1 RESULT");
+        page2.SetText("PAINU HIITEEN, PUMMI!");
         page2.SetLast();
 
-        page3.SetText("DIALOG PAGE 2");
-        page3.SetReply(0, "REPLY1", page4);
-        page3.SetReply(1, "REPLY2", page5);
+        page3.SetText("Voi että, et uskokaan, kun minulla on ollut niin rankkaa… Bisness pyöri ja elämä sujui.. Mutta nyt on kaikki kuviot kuule kusseet ja pahasti…");
+        page3.SetReply(0, "Lähde pois", page4);
+        page3.SetReply(1, "Jatka kuuntelua", page5);
+        page3.SetReply(3, "Sano jotain myötätuntoista", page6);
 
-        page4.SetText("D2 REPLY1 RESULT");
+        page4.SetText("Et saanut rahaa");
         page4.SetLast();
 
-        page5.SetText("D2 REPLY2 RESULT");
-        page5.SetLast();
+// Andrei antaa 20e:
+        page6.SetText("...ensin puukottaa liikekumppanit selkään, vaikka monenmoista jeesiä hänelle tarjosin ja koulutin kuule sen pikkuvesselistä alalle, oli mulle kuin oma poika.. Ja vaimokin saamari läx sen matkaan ja koirakin perkele karkasi… Kuulee… Sinä.. Pate? Sinä oot kuule hyvä jätkä, Pate! Ota kuule tästä, saat kakskymppiä!");
+        page6.SetLast();
+
+// Andrei ryöstää:
+        page6.SetText("JUMANSVIIDU MIKÄ MIELISTELIJÄ! RAHAT TÄNNE");
+        page6.SetLast();
 
         AdvanceDialog(page1);
     }
