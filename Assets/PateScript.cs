f636ef94f0f12dd2d8fc71f595654df6d6b0cc98
villelin <villelin@metropolia.fi>
2016-09-28 12:14:30 +0300

    Paten liike fysiikalla

--- Assets/PateScript.cs
+++ Assets/PateScript.cs
@@ -5,23 +5,30 @@ public class PateScript : MonoBehaviour
 {
     SpriteRenderer renderer;
     Vector2 target_pos;
-    Vector2 current_pos;
     GameObject target_object;
     GameObject game_controller;
+    Rigidbody2D pate_physics;
     float angle;
-    const float speed = 5.0f;
+    const float speed = 100.0f;
+    bool walking;
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
         renderer = GameObject.Find("Pate").GetComponent<SpriteRenderer>();
         game_controller = GameObject.Find("GameController");
         angle = 0.0f;
 
+        pate_physics = GameObject.Find("Pate").GetComponent<Rigidbody2D>();
+
         target_object = null;
+
+        walking = false;
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
         float angrad = angle * 180.0f / Mathf.PI;
         float x = Mathf.Cos(angrad);
         float y = Mathf.Sin(angrad);
@@ -33,26 +40,23 @@ public class PateScript : MonoBehaviour
         float walk_offset = 0.0f;
         float walk_rot = 0.0f;
 
-        Vector2 direction = target_pos - current_pos;
-        if (direction.magnitude > speed)
+        Vector2 direction = target_pos - pate_physics.position;
+        if (direction.magnitude < 5.0f)
         {
-            direction.Normalize();
-            direction *= speed;
-            direction.y *= 0.3f;
+            // close enough to target, let's stop
+            StopMoving();
+        }
 
+        //transform.position = new Vector3(current_pos.x, current_pos.y + walk_offset, 0);
+
+        renderer.sortingOrder = 1000 - (int)Mathf.Floor(transform.position.y);
+
+        // animate pate if walking
+        if (walking)
+        {
             walk_offset = Mathf.Abs(Mathf.Sin(angrad)) * 15.0f;
             walk_rot = 0.0f - Mathf.Sin(angrad) * 20.0f;
         }
-        else
-        {
-            // close enough to target, let's stop
-            direction = new Vector2(0, 0);
-        }
-
-        current_pos += direction;
-        transform.position = new Vector3(current_pos.x, current_pos.y + walk_offset, 0);
-
-        renderer.sortingOrder = 1000 - (int)Mathf.Floor(transform.position.y);
 
         transform.localRotation = Quaternion.Euler(0, 0, walk_rot);
     }
@@ -62,31 +66,41 @@ public class PateScript : MonoBehaviour
         Debug.Log("SET TARGET " + pos);
         target_pos = pos;
 
-        Vector2 direction = target_pos - current_pos;
+        Vector2 direction = target_pos - pate_physics.position;
+        direction.Normalize();
 
-
-        RaycastHit2D hit = Physics2D.Raycast(current_pos, direction, direction.magnitude);
-        if (hit.collider != null)
-        {
-            target_pos = hit.point;
-        }
-
+        pate_physics.velocity = direction * speed;
 
         
 		if (direction.x < 0.0f)
 			renderer.flipX = false;
 		else
 			renderer.flipX = true;
+
+        walking = true;
 	}
 
     void SetPosition(Vector2 pos)
     {
-        current_pos = pos;
-        target_pos = current_pos;
+        target_pos = pos;
+
+        pate_physics.position = pos;
+        pate_physics.velocity = new Vector2(0, 0);
+        walking = false;
     }
 
     void StopMoving()
     {
-        target_pos = current_pos;
+        target_pos = pate_physics.position;
+
+        pate_physics.velocity = new Vector2(0, 0);
+        walking = false;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("törmättiin");
+
+        StopMoving();
     }
 }
