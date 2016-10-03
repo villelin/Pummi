48e35f8c4ef09e14f86952668c8416430f6f88f7
villelin <villelin@metropolia.fi>
2016-10-03 12:25:03 +0300

    rahat animaatiolla

--- Assets/ConversationController.cs
+++ Assets/ConversationController.cs
@@ -17,6 +17,12 @@ public class ConversationController : MonoBehaviour
     GameObject inspector_image;
     GameObject martta_image;
 
+    GameObject cash_bonus;
+    Text cash_bonus_text;
+
+    private float cash_bonus_timer;
+    private const float cash_bonus_duration = 3.0f;
+
 	// Use this for initialization
 	void Start ()
     {
@@ -45,6 +51,12 @@ public class ConversationController : MonoBehaviour
         inspector_image = GameObject.Find("InspectorImage");
         martta_image = GameObject.Find("MarttaImage");
 
+        cash_bonus = GameObject.Find("CashBonusText");
+        cash_bonus_text = GameObject.Find("CashBonusText").GetComponent<Text>();
+        cash_bonus.SetActive(false);
+
+        cash_bonus_timer = 0;
+
         conversation_target = Persistence.instance.conversation_target;
 
         if (conversation_target != null)
@@ -59,7 +71,16 @@ public class ConversationController : MonoBehaviour
     // Update is called once per frame
     void Update ()
     {
-	
+        if (cash_bonus_timer > 0)
+        {
+            cash_bonus_timer -= Time.deltaTime;
+
+
+            float pos = Mathf.Max(cash_bonus_timer / cash_bonus_duration, 0.0f);
+            cash_bonus_text.color = new Color(0.0f, 1.0f, 0.0f, pos);
+
+            cash_bonus.transform.localPosition = new Vector3(0, -150 + ((1.0f - pos) * 300.0f), 0);
+        }
 	}
 
     void SetDialog(DialogPage page)
@@ -118,8 +139,17 @@ public class ConversationController : MonoBehaviour
 
         if (page.GetReward() != 0)
         {
-            // TODO: animation
-            Persistence.instance.player.AddCash(page.GetReward());
+            double reward = page.GetReward();
+
+            if (reward > 0.0)
+            {
+                cash_bonus_timer = cash_bonus_duration;
+                cash_bonus.SetActive(true);
+
+                cash_bonus_text.text = string.Format("+€ {0:0.00}", page.GetReward());
+            }
+
+            Persistence.instance.player.AddCash(reward);
         }
     }
 
