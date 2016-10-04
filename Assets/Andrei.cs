d95a6a36c082561b7d7a0a6a0a53e9f6b236cf74
Raezla <affaokami@hotmail.com>
2016-10-04 09:58:05 +0300

    Random Andrei
    Andrei ryöstää 40% todennäköisyydellä, kun hänelle menee puhumaan!

--- Assets/Andrei.cs
+++ Assets/Andrei.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 
 public class Andrei : NPC
@@ -7,7 +8,8 @@ public class Andrei : NPC
     DialogPage page3;
     DialogPage page4;
     DialogPage page5;
-    DialogPage page6;
+	DialogPage page6;
+	DialogPage page7;
 
     public Andrei() : base("Andrei")
     {
@@ -17,32 +19,48 @@ public class Andrei : NPC
         page4 = new DialogPage(DialogPageImage.Andrei);
 		page5 = new DialogPage(DialogPageImage.Andrei);
 		page6 = new DialogPage(DialogPageImage.AndreiAngry);
+		page7 = new DialogPage (DialogPageImage.AndreiAngry);
 
-        page1.SetText("Jahashh... Mikässhh miässh she shinä olethh?");
-        page1.SetReply(0, "Pate vaan, tarvin rahaa", page2);
-        page1.SetReply(1, "Olen Pate, entäs sinä?", page3);
+		Random rnd = new Random();
 
-        page2.SetText("PAINU HIITEEN, PUMMI!");
-        page2.SetLast("Okei...");
+		double n;
 
-        page3.SetText("Voi että, et uskokaan, kun minulla on ollut niin rankkaa… Bisness pyöri ja elämä sujui.. Mutta nyt on kaikki kuviot kuule kusseet ja pahasti…");
-        page3.SetReply(0, "Lähde pois", page4);
-        page3.SetReply(1, "Jatka kuuntelua", page5);
-        page3.SetReply(3, "Sano jotain myötätuntoista", page6);
+		n = rnd.NextDouble ();
 
-        page4.SetText("Et saanut rahaa");
-        page4.SetLast("HÖH");
+		if (n < 0.4) {
+			page7.SetText ("JUMANSVIIDU MIKÄ MIELISTELIJÄ! RAHAT TÄNNE");
+			page7.SetLast ("APUAAA");
+			page7.SetReward (-float.MaxValue);
+
+			AdvanceDialog (page7);
+
+		} else {
+			page1.SetText ("Jahashh... Mikässhh miässh she shinä olethh?");
+			page1.SetReply (0, "Pate vaan, tarvin rahaa", page2);
+			page1.SetReply (1, "Olen Pate, entäs sinä?", page3);
+
+			page2.SetText ("PAINU HIITEEN, PUMMI!");
+			page2.SetLast ("Okei...");
+
+			page3.SetText ("Voi että, et uskokaan, kun minulla on ollut niin rankkaa… Bisness pyöri ja elämä sujui.. Mutta nyt on kaikki kuviot kuule kusseet ja pahasti…");
+			page3.SetReply (0, "Lähde pois", page4);
+			page3.SetReply (1, "Jatka kuuntelua", page5);
+			page3.SetReply (3, "Sano jotain myötätuntoista", page6);
+
+			page4.SetText ("Et saanut rahaa");
+			page4.SetLast ("HÖH");
 
 // Andrei antaa 20e:
-        page5.SetText("...ensin puukottaa liikekumppanit selkään, vaikka monenmoista jeesiä hänelle tarjosin ja koulutin kuule sen pikkuvesselistä alalle, oli mulle kuin oma poika.. Ja vaimokin saamari läx sen matkaan ja koirakin perkele karkasi… Kuulee… Sinä.. Pate? Sinä oot kuule hyvä jätkä, Pate! Ota kuule tästä, saat kakskymppiä!");
-        page5.SetLast("JES!");
-        page5.SetReward(20.0);
+			page5.SetText ("...ensin puukottaa liikekumppanit selkään, vaikka monenmoista jeesiä hänelle tarjosin ja koulutin kuule sen pikkuvesselistä alalle, oli mulle kuin oma poika.. Ja vaimokin saamari läx sen matkaan ja koirakin perkele karkasi… Kuulee… Sinä.. Pate? Sinä oot kuule hyvä jätkä, Pate! Ota kuule tästä, saat kakskymppiä!");
+			page5.SetLast ("JES!");
+			page5.SetReward (20.0);
 
 // Andrei ryöstää:
-        page6.SetText("JUMANSVIIDU MIKÄ MIELISTELIJÄ! RAHAT TÄNNE");
-        page6.SetLast("APUAAA");
-        page6.SetReward(-float.MaxValue);
+			page6.SetText ("JUMANSVIIDU MIKÄ MIELISTELIJÄ! RAHAT TÄNNE");
+			page6.SetLast ("APUAAA");
+			page6.SetReward (-float.MaxValue);
 
-        AdvanceDialog(page1);
+			AdvanceDialog (page1);
+		}
     }
 }
